4898e693c1c98848649e450388edcd77831dac38
V3RNE42 <vatuviii@hotmail.com>
2022-04-26 14:21:15 +0200

    actualizo a las 14:20

--- 21-ABRIL-2022.js
+++ 21-ABRIL-2022.js
@@ -11,3 +11,78 @@ function sumar() {
     document.getElementById("resultado").style.color="red";
     document.getElementById("resultado").style.backgroundColor="blue";
 }
+let resultado=0;
+var contPantalla=0;
+let contOperador=0;
+let contPunto=0;
+let arregloPantalla="";
+let pantalla="";
+var ultimovalor;
+let numero=[0,0,0];
+let operador=false;
+
+function inserta(tecla) {
+    switch (tecla) {
+        case '/':
+        case 'x':
+        case '-':
+        case '+':
+            contOperador++;
+            operador=true;
+            contPunto=0;
+            break;
+
+        case '.':
+            contPunto;
+            break;
+    
+        default:
+            break;
+    }
+    if (contPunto<=1 && contOperador<=1 && contPantalla<15) {
+        arregloPantalla=arregloPantalla+""+tecla;
+        document.getElementById("pantSup").innerHTML=arregloPantalla;
+        contPantalla++;
+    }
+    ultimovalor=tecla;
+};
+
+function borraPantalla() {
+    contPantalla=0;
+    contPunto=0;
+    contOperador=0;
+    operador=false;
+    arregloPantalla="";
+    document.getElementById("pantSup").innerHTML=arregloPantalla;
+    document.getElementById("pantInf").innerHTML=arregloPantalla;
+};
+
+function imprimeResultado() {
+    borraPantalla();
+};
+
+function invierte() {};
+
+function elimina() {
+    contPantalla-=1;
+    contPunto=0;
+    contOperador=0;
+    let copiaPantalla="";
+    for (let index = 0; index < arregloPantalla.length-1; index++) {
+        copiaPantalla+=arregloPantalla[index];
+        if (arregloPantalla[index]==='.') contPunto++;
+        if (arregloPantalla[index]==='%' || arregloPantalla[index]==='1/x' ||
+            arregloPantalla[index]=='x^2' || arregloPantalla[index]=='RAIZ' ||
+            arregloPantalla[index]=='/' || arregloPantalla[index]=='x' ||
+            arregloPantalla[index]=='-' || arregloPantalla[index]=='+') {
+                contOperador++;
+            }
+    }
+    arregloPantalla=copiaPantalla;
+    document.getElementById("pantSup").innerHTML=arregloPantalla;
+};
+
+function expone() {};
+
+function raiz() {};
+
